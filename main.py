bce800f56c8bdd63c174241fbf42e9e2b6243ff7
Debanshi123050 <123050@eesd.org>
2021-02-11 23:41:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Have the computer choose a random number between 1-10 and give them a fortune:
+import random
+fortune = random.randint(1,12)
+
+print("Fortune Teller group number 2")
+print(" ")
+
+if (fortune == 1):
+  print('You will soon see that your greatest wish will come true')
+elif (fortune == 2):
+  print('A smile is your personal welcome mat')
+elif (fortune == 3):
+  print('A friend is a present you give yourself')  
+elif (fortune == 4):
+  print('If you are determined to live life to the fullest, have a hamburger tonight.')  
+elif (fortune == 5):
+  print('If you eat a cookie for breakfast tomorrow, you will make a new friend')  
+elif (fortune == 6):
+  print('Should you drink your favorite drink 3 days in a row at 8:00am on a July, you will find youself with more moments of bliss.')  
+elif (fortune == 7):
+  print('One day soon, at sunset, you will find youself content because your greatest dream has been realized.')
+elif (fortune == 8):
+  print('Soon life will become more interesting if you find a coin near the donut shop closest to your house')  
+elif (fortune == 9):
+  print('An inch of time is an inch of extra churro')  
+elif (fortune == 10):
+  print('Bide your time, for success is near.. assuming, you have your daily coffee')
+elif (fortune == 11):
+  print('Better ask twice than lose yourself once; Ask a cookie if you cant ask anyone else.')  
+elif (fortune == 12):
+  print("Don't eat dessert before dinner if you value your favorite pencil.")     
